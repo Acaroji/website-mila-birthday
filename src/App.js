d4d493b5c3375bbe4775caf8ee15e8a5430b2335
Ashley Caro <ashleycaro93@gmail.com>
2019-09-01 21:13:05 +0200

    css changes

--- src/App.js
+++ src/App.js
@@ -5,27 +5,25 @@ function App() {
   return (
     
     <div className="App">
-      {/* <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta> */}
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
       <div className="landingPage">
         <div className="topLandingPAge">
           <div className="nombreMila">MILA</div>
-          <div className="pensamientosMila">60 pensees pour Mila</div>
+          <div className="pensamientosMila">60 pensées pour Mila</div>
         </div>
         <div className="landingBody">
-          {/* <div className="miniNavbar">
-            <ul className="navBar">
-              <li>Invitation</li>
-              <li>Confirmation </li>
-            </ul>
-          </div> */}
+          <div className="miniNavbar">
+            <ul className="navBar"></ul>
+          </div>
           <img src="./images/MilaNinos2.jpg" height="250px" weigth="250px" alt="family"/>
           <div className="information">
-            Sabado 5 de Octubre 2019
+             Samedi 5 Octobre 2019, 
+             Nous surprendrons Mila pour ses 60 ans à Auvers sur Oise
           </div>
-          <img src="./images/dry-leaf.png" alt="hoja" />
-          <div className="messageBottom"> Vamos todos a celebrar la vida de nuestra querida Mila</div>
+          <img src="./images/dry-leaf1.png" alt="hoja" />
+          <div className="messageBottom"> wfw</div>
           <div className="confirmation">
-            <a href="https://forms.gle/NfxJTqnuWzhw8z3N6"> <button className="button"> confirmacion</button></a> 
+            <a href="https://forms.gle/NfxJTqnuWzhw8z3N6"> <button className="button"> Confirmer ma participation</button></a> 
           </div> 
         </div>
       </div>
